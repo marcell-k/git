66d63d9e56defce36f4084a4cd88078653ecee5d
TestName <TestNmae@email.com>
2026-08-24 20:51:16 +0100

    refactor(write_commit): set NAME/EMAIL and time

--- src/commands/commit_tree.rs
+++ src/commands/commit_tree.rs
@@ -1,7 +1,9 @@
 use crate::objects::{Kind, Object};
 use anyhow::Context;
+use std::env;
 use std::fmt::Write;
 use std::io::Cursor;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 pub(crate) fn write_commit(
     message: &str,
@@ -13,10 +15,29 @@ pub(crate) fn write_commit(
     if let Some(parent_hash) = parent_hash {
         writeln!(commit, "parent {parent_hash}")?
     }
-    writeln!(commit, "author ....")?;
-    writeln!(commit, "committer ....")?;
+    let (name, email) =
+        if let (Some(name), Some(email)) = (env::var_os("NAME"), env::var_os("EMAIL")) {
+            let name = name
+                .into_string()
+                .map_err(|_| anyhow::anyhow!("$NAME is not set"))?;
+            let email = email
+                .into_string()
+                .map_err(|_| anyhow::anyhow!("$EMAIL is not set"))?;
+
+            (name, email)
+        } else {
+            (String::from("Author name"), String::from("email"))
+        };
+    let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .context("system clock before unix epoch")?
+        .as_secs();
+
+    writeln!(commit, "author {name} <{email}> {now} +0100")?;
+    writeln!(commit, "committer {name} <{email}> {now} +0100")?;
     writeln!(commit)?;
     writeln!(commit, "{message}")?;
+
     Object {
         kind: Kind::Commit,
         expected_size: commit.len() as u64,
